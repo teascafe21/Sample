81d6f9f35ffc2437dc1ad11522eb887e827aee97
teascafe21 <asteras@redcave.net>
2023-04-25 14:21:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import tkinter as tk
 
+# ウィンドウの作成
 root = tk.Tk()
+root.geometry("300x200")
+
+# ボタンの作成
+button = tk.Button(root, text="クリックしてください")
+
+# ボタンの配置
+button.pack()
+
+# ウィンドウの表示
 root.mainloop()
